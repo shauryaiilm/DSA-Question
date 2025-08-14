816a03485c496be9ef4fd2cdd30a8454c36ae7fd
shauryaiilm <shaurya.srivastava.cs28@iilm.edu>
2025-08-14 12:30:19 +0530

    range between start year and end year

--- Lab2/LeapYear.java
+++ Lab2/LeapYear.java
@@ -1,17 +1,29 @@
 import java.util.Scanner;
 
 public class LeapYear {
+    public static boolean isLeapYear(int year) {
+        return (year % 400 == 0) || (year % 4 == 0 && year % 100 != 0);
+    }
+
+    public static void printLeapYears(int start, int end) {
+        System.out.println("ALL leap years: \n");
+        for (int year = start; year <= end; year++) {
+            if (isLeapYear(year)) {
+                System.out.print(year + " ");
+            }
+        }
+        System.out.println();
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter a year: ");
-        int year = sc.nextInt();
+        System.out.print("Enter start year: ");
+        int start = sc.nextInt();
+        System.out.print("Enter end year: ");
+        int end = sc.nextInt();
 
-        if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) {
-            System.out.println("It is a leap Year");
-        } else {
-            System.out.println("It is NOT a Leap Year");
-        }
+        printLeapYears(start, end);
 
         sc.close();
     }
